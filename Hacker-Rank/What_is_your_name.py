f8a88d7a4675ee904907be90763601fd72d93958
Solomon <60452720+Solomon-Awoke@users.noreply.github.com>
2024-09-24 17:20:08 +0300

    What is your name ?

---
+++ Hacker-Rank/What_is_your_name.py
@@ -0,0 +1,12 @@
+
+def print_full_name(first_name, last_name):
+    
+    full_name = f"Hello {first_name} {last_name} ! You just delved into python."
+    
+    print(full_name)
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+    
